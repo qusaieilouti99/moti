c27d9c8aea6fabbe8a5c51d47e8a06c571ac222a
Fernando Rojo <frojo@sas.upenn.edu>
2021-01-31 20:33:24 -0500

    refactor: rename to moti

---
+++ src/motify/use-animator.ts
@@ -0,0 +1,212 @@
+import { useRef } from 'react'
+import Animated, { useSharedValue } from 'react-native-reanimated'
+
+type InternalControllerState<V> = number | V[keyof V]
+
+type Controller<V> = {
+  /**
+   * A hook to synchronously read the current animation state.
+   *
+   * ```js
+   * const animator = useAnimator({
+   *   hidden: { opacity: 0 },
+   *   shown: { opacity: 1 }
+   * })
+   *
+   * const onPress = () => {
+   *   if (animator.current === 'hidden') {
+   *     animator.transitionTo('shown')
+   *   } else {
+   *     animator.transitionTo('hidden')
+   *   }
+   * }
+   * ```
+   *
+   * Do not mutate the `current` value directly; this will break. Instead, use the `transitionTo` function.
+   */
+  current: null | keyof V
+  /**
+   * @private
+   * Internal state used to drive animations. You shouldn't use this. Use `.current` instead to read the current state. Use `transitionTo` to edit it.
+   */
+  __state: Animated.SharedValue<any>
+  /**
+   * Transition to another state, as defined by this hook.
+   *
+   * ```js
+   * const animator = useAnimator({
+   *   hidden: { opacity: 0 },
+   *   shown: { opacity: 1 }
+   * })
+   *
+   * const onPress = () => {
+   *   if (animator.current === 'hidden') {
+   *     animator.transitionTo('shown')
+   *   } else {
+   *     animator.transitionTo('hidden')
+   *   }
+   * }
+   * ```
+   */
+  transitionTo: (key: keyof V | ((currentState: keyof V) => keyof V)) => void
+}
+
+/**
+ *
+ * `useAnimatedState` lets you control your animation state, based on static presets. It is the most performant way to drive animations.
+ *
+ * @param variants specify your style variants.
+ * @param config Optionally define your from variant key.
+ *
+ * **Example**
+ *
+ * ```jsx
+ * const animator = useAnimatedState({
+ *   from: {
+ *     opacity: 0
+ *   },
+ *   open: {
+ *     opacity: 1
+ *   },
+ *   pressed: {
+ *     opacity: 0.7
+ *   }
+ * })
+ *
+ * return (
+ *   <>
+ *     <Drip.View state={animator} />
+ *     <Button
+ *      title="Change!"
+ *      onPressIn={() => {
+ *        animator.transitionTo('pressed')
+ *      }}
+ *      onPressOut={() => animator.transitionTo('open')}
+ *     />
+ *   </>
+ * )
+ * ```
+ *
+ * You can also use it with a access the `current` state:
+ *
+ * ```jsx
+ * const animator = useAnimatedState({
+ *   from: {
+ *     opacity: 0
+ *   },
+ *   open: {
+ *     opacity: 1
+ *   },
+ *   pressed: {
+ *     opacity: 0.7
+ *   }
+ * })
+ *
+ * return (
+ *   <>
+ *     <Drip.View state={animator} />
+ *     <Button
+ *      title="Change!"
+ *      onPress={() => {
+ *        if (animator.current === 'from') {
+ *          animator.animateTo('open')
+ *        } else {
+ *          animator.animateTo('from')
+ *        }
+ *      }}
+ *     />
+ *   </>
+ * )
+ * ```
+ *
+ * If you provide an `from` key, this will be your default starting variant. If you don't, however, you can use the second argument to specify the from state. If you do not, then there will be no animated style to begin with (this is okay, as long as you intended it.)
+ *
+ * ```jsx
+ * const animator = useAnimatedState({
+ *   from: {
+ *     opacity: 0
+ *   },
+ *   open: {
+ *     opacity: 1
+ *   },
+ * }, { from: 'from' })
+ * ```
+ *
+ * **Note** if you change variants on the fly by updating state, they will not re-render. This is to maintain good performance. Instead, you should pre-define all states in the first render. Then use `animator.transitionTo` to change state, and `animator.current` to read the state.
+ *
+ * This means the `useAnimatedState` hook should only be used with static states. If you need dynamic states, please use the `animate` prop directly.
+ *
+ * **Minor suggestion**
+ *
+ * As a rule of thumb, don't destructure the `animator`. `animator` has a stable reference, but the values inside of it do not.
+ *
+ * ```js
+ * // ✅ in general, do this
+ * const animator = useAnimatedState(...)
+ *
+ * useEffect(() => {
+ *  if (loading) animator.transitionTo('some-state')
+ *  else animator.transitionTo('some-other-state')
+ * }, [animator, loading])
+ *
+ * // 🚨 not this
+ * const { current, transitionTo } = useAnimator(...)
+ *
+ * useEffect(() => {
+ *  if (loading) transitionTo('some-state')
+ *  else transitionTo('some-other-state')
+ * }, [transitionTo, loading])
+ * ```
+ *
+ * You don't have to follow that suggestion if you don't want to. But I recommend it to prevent unintended consequences of triggering effects when these are used in dependency arrays.
+ *
+ * If you aren't using the animator in a dependency array anywhere, then you can ignore this suggestion. But I treat it as a rule of thumb to keep things simpler.
+ *
+ * Technically, it's fine if you do this with `transitionTo`. It's `current` you'll want to watch out for, since its reference will change, without triggering re-renders. This functions similar to `useRef`.
+ */
+export default function useAnimatedState<V>(
+  _variants: V,
+  { from = 'from' as keyof V }: UseAnimatorConfig<V> = {}
+) {
+  const controller = useRef<Controller<V>>()
+  const __state = useSharedValue<InternalControllerState<V>>(
+    from ? _variants[from] : 0,
+    false // don't rebuild it
+  )
+
+  const selectedVariant = useRef(from)
+  const variants = useRef(_variants)
+
+  if (controller.current == null) {
+    controller.current = {
+      __state,
+      transitionTo(nextStateOrFunction) {
+        const runTransition = (nextState: keyof V) => {
+          const value = variants.current[nextState]
+
+          selectedVariant.current = nextState
+
+          __state.value = value
+        }
+
+        if (typeof nextStateOrFunction === 'function') {
+          // similar to setState, let people compose a function that takes in the current value and returns the next one
+          runTransition(nextStateOrFunction(this.current as keyof V))
+        } else {
+          runTransition(nextStateOrFunction)
+        }
+      },
+      get current(): keyof V {
+        return selectedVariant.current
+      },
+    }
+  }
+
+  return controller.current as Controller<V>
+}
+
+type UseAnimatorConfig<V, InitialKey = keyof V> = {
+  from?: InitialKey
+}
+
+export type UseAnimator<V> = Controller<V>
---
+++ src/motify/use-map-animate-to-style.ts
@@ -0,0 +1,463 @@
+import { usePresence } from 'framer-motion'
+import { useEffect } from 'react'
+import type { TransformsStyle } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+  withSpring,
+  withTiming,
+  withDelay,
+  withRepeat,
+  withSequence,
+  runOnJS,
+} from 'react-native-reanimated'
+import { PackageName } from '../constants/package-name'
+import type { DripsifyProps, Transforms, TransitionConfig } from './types'
+
+const isColor = (styleKey: string) => {
+  'worklet'
+  return [
+    'backgroundColor',
+    'borderBottomColor',
+    'borderColor',
+    'borderEndColor',
+    'borderLeftColor',
+    'borderRightColor',
+    'borderStartColor',
+    'borderTopColor',
+    'color',
+  ].includes(styleKey)
+}
+
+const isTransform = (styleKey: string) => {
+  'worklet'
+  const transforms: (keyof Transforms)[] = [
+    'perspective',
+    'rotate',
+    'rotateX',
+    'rotateY',
+    'rotateZ',
+    'scale',
+    'scaleX',
+    'scaleY',
+    'translateX',
+    'translateY',
+    'skewX',
+    'skewY',
+  ]
+  return transforms.includes(styleKey as keyof Transforms)
+}
+function animationDelay<Animate>(
+  styleProp: string,
+  transition: DripsifyProps<Animate>['transition'],
+  defaultDelay?: number
+) {
+  'worklet'
+  let delayMs: TransitionConfig['delay'] = defaultDelay
+
+  if ((transition as any)?.[styleProp as keyof Animate]?.delay != null) {
+    delayMs = (transition as any)?.[styleProp as keyof Animate]?.delay
+  } else if (transition?.delay != null) {
+    delayMs = transition.delay
+  }
+
+  return {
+    delayMs,
+  }
+}
+
+function animationConfig<Animate>(
+  styleProp: string,
+  transition: DripsifyProps<Animate>['transition']
+) {
+  'worklet'
+
+  const key = styleProp
+  let repeatCount = 0
+  let repeatReverse = true
+
+  let animationType: Required<TransitionConfig>['type'] = 'spring'
+  if (isColor(key) || key === 'opacity') animationType = 'timing'
+
+  // say that we're looking at `width`
+  // first, check if we have transition.width.type
+  if ((transition as any)?.[key as keyof Animate]?.type) {
+    animationType = (transition as any)[key]?.type
+  } else if (transition?.type) {
+    // otherwise, fallback to transition.type
+    animationType = transition.type
+  }
+
+  if ((transition as any)?.[key as keyof Animate]?.loop) {
+    repeatCount = Infinity
+  } else if (transition?.loop) {
+    // otherwise, fallback to transition.type
+    repeatCount = Infinity
+  }
+
+  if ((transition as any)?.[key as keyof Animate]?.repeat != null) {
+    repeatCount = (transition as any)?.[key as keyof Animate]?.repeat
+  } else if (transition?.repeat) {
+    repeatCount = transition.repeat
+  }
+
+  if ((transition as any)?.[key as keyof Animate]?.repeatReverse != null) {
+    repeatReverse = (transition as any)?.[key as keyof Animate]?.repeatReverse
+  } else if (transition?.repeatReverse) {
+    repeatReverse = transition.repeatReverse
+  }
+
+  let config = {}
+  let animation: Function = () => 1
+
+  if (animationType === 'timing') {
+    const duration =
+      ((transition as any)?.[key as keyof Animate] as Animated.WithTimingConfig)
+        ?.duration ?? (transition as Animated.WithTimingConfig)?.duration
+
+    const easing =
+      ((transition as any)?.[key as keyof Animate] as Animated.WithTimingConfig)
+        ?.easing ?? (transition as Animated.WithTimingConfig)?.easing
+
+    if (easing) {
+      config['easing'] = easing
+    }
+    if (duration != null) {
+      config['duration'] = duration
+    }
+    animation = withTiming
+  } else if (animationType === 'spring') {
+    animation = withSpring
+    config = {
+      // solve the missing velocity bug in 2.0.0-rc.0
+      velocity: 2,
+    } as Animated.WithSpringConfig
+    const configKeys: (keyof Animated.WithSpringConfig)[] = [
+      'damping',
+      'mass',
+      'overshootClamping',
+      'restDisplacementThreshold',
+      'restSpeedThreshold',
+      'stiffness',
+      'velocity',
+    ]
+    configKeys.forEach((configKey) => {
+      'worklet'
+      const styleSpecificConfig = transition?.[key]?.[configKey]
+      const transitionConfigForKey = transition?.[configKey]
+
+      if (styleSpecificConfig != null) {
+        config[configKey] = styleSpecificConfig
+      } else if (transitionConfigForKey != null) {
+        config[configKey] = transitionConfigForKey
+      }
+    })
+  } else if (animationType === 'decay') {
+    // TODO this doesn't work for now
+    if (__DEV__) {
+      console.error(
+        `[${PackageName}]: You passed transition type: decay, but this isn't working for now. Honestly, not sure why yet. Try passing other transition fields, like clamp, velocity, and deceleration. If that solves it, please open an issue and let me know.`
+      )
+    }
+    animation = withDecay
+    config = {
+      velocity: 2,
+      deceleration: 2,
+    }
+    const configKeys: (keyof Animated.WithDecayConfig)[] = [
+      'clamp',
+      'velocity',
+      'deceleration',
+    ]
+    configKeys.forEach((configKey) => {
+      'worklet'
+      // is this necessary ^ don't think so...?
+      const styleSpecificConfig = transition?.[key]?.[configKey]
+      const transitionConfigForKey = transition?.[configKey]
+
+      if (styleSpecificConfig != null) {
+        config[configKey] = styleSpecificConfig
+      } else if (transitionConfigForKey != null) {
+        config[configKey] = transitionConfigForKey
+      }
+    })
+  }
+
+  return {
+    animation,
+    config,
+    repeatReverse,
+    repeatCount,
+    shouldRepeat: !!repeatCount,
+  }
+}
+
+export default function useMapAnimateToStyle<Animate>({
+  animate,
+  from = false,
+  transition,
+  delay: defaultDelay,
+  state,
+  stylePriority = 'state',
+  onDidAnimate,
+  exit,
+}: DripsifyProps<Animate>) {
+  const isMounted = useSharedValue(false, false)
+  const [isPresent, safeToUnmount] = usePresence()
+
+  // is any of this necessary?
+  const initialSV = useSharedValue(from)
+  const animateSV = useSharedValue(animate)
+  const exitSV = useSharedValue(exit)
+  const hasExitStyle = typeof exit === 'object' && !!Object.keys(exit).length
+
+  const style = useAnimatedStyle(() => {
+    const final = {
+      // initializing here fixes reanimated object.__defineProperty bug(?)
+      transform: [] as TransformsStyle['transform'],
+    }
+    const variantStyle: Animate = state?.__state?.value || {}
+
+    const animateStyle = animateSV.value || {}
+    const initialStyle = initialSV.value || {}
+    const exitStyle = exitSV.value || {}
+
+    const isExiting = !isPresent && hasExitStyle
+
+    let mergedStyles: Animate
+    if (stylePriority === 'state') {
+      mergedStyles = { ...animateStyle, ...variantStyle }
+    } else {
+      mergedStyles = { ...variantStyle, ...animateStyle }
+    }
+
+    if (isExiting) {
+      mergedStyles = exitStyle as any
+    }
+
+    Object.keys(mergedStyles).forEach((key, index) => {
+      'worklet'
+
+      const initialValue = initialStyle[key]
+      const value = mergedStyles[key]
+
+      const {
+        animation,
+        config,
+        shouldRepeat,
+        repeatCount,
+        repeatReverse,
+      } = animationConfig(key, transition)
+
+      const callback: (canceled: boolean, value?: any) => void = (
+        canceled,
+        recentValue
+      ) => {
+        if (onDidAnimate) {
+          runOnJS(onDidAnimate)(key as any, canceled, recentValue)
+        }
+        if (isExiting) {
+          // if this is true, then we've finished our exit animations
+          const isLastStyleKeyToAnimate =
+            index + 1 === Object.keys(mergedStyles).length
+
+          if (isLastStyleKeyToAnimate && safeToUnmount) {
+            runOnJS(safeToUnmount)()
+          }
+        }
+      }
+
+      if (initialValue != null) {
+        // if we haven't mounted, or if there's no other value to use besides the initial one, use it.
+        if (isMounted.value === false || value == null) {
+          if (isTransform(key) && final.transform) {
+            // this syntax avoids reanimated .__defineObject error
+            const transform = {}
+            transform[key] = animation(initialValue, config)
+
+            // final.transform.push({ [key]: initialValue }) does not work!
+            // @ts-ignore
+            final.transform.push(transform)
+            // console.log({ final })
+          } else {
+            final[key] = animation(initialValue, config)
+          }
+          return
+        }
+      }
+
+      let { delayMs } = animationDelay(key, transition, defaultDelay)
+
+      if (isColor(key)) {
+        // TODO: FIX THIS
+        // if (__DEV__) {
+        if (
+          typeof value === 'string' &&
+          !value.startsWith('rgb') &&
+          !value.startsWith('#')
+        ) {
+          console.error(
+            `[${PackageName}]: You passed ${key}: ${value}, but not all color values are supported yet in Reanimated 2. ☹️ 
+                  
+Please use an rgb or hex formatted color.
+
+  Please go to https://github.com/software-mansion/react-native-reanimated/issues/845 and comment so that this bug can get fixed!`
+          )
+        }
+        // }
+        console.log('[color]', { key, value })
+      }
+
+      if (value == null || value === false) {
+        // skip missing values
+        // this is useful if you want to do {opacity: loading && 1}
+        // without this, those values will break I think
+        return
+      }
+      if (Array.isArray(value)) {
+        // we have a sequence
+
+        /**
+         * There is some code duplication in this section and in the ones below.
+         *
+         * However, I prefer this for open source. It makes it easier for collaborators to identify issues without a million wrappers.
+         *
+         * If there's something *obvious* that would benefit from abstraction, we can. But let's keep it simple.
+         */
+
+        // remove null, false values to allow for conditional styles
+        const sequence = value
+          .filter((step) => {
+            if (typeof step === 'object') {
+              return step?.value != null && step?.value !== false
+            }
+            return step != null && step !== false
+          })
+          .map((step) => {
+            let stepDelay = delayMs
+            let stepValue = step
+            let stepConfig = { ...config }
+            let stepAnimation = animation
+            if (typeof step === 'object') {
+              // TODO this should spread from step, but reanimated won't allow this on JS thread?
+              // const { delay, value, ...transition } = step
+              const transition = step
+              const { delay, value } = step
+
+              // @ts-ignore TODO use this later, it currently breaks reanimated :(
+              const { config: customConfig, animation } = animationConfig(
+                key,
+                transition
+              )
+
+              stepConfig = {
+                ...stepConfig,
+                //  ...customConfig
+              }
+              stepAnimation = animation
+              if (delay != null) {
+                stepDelay = delay
+              }
+              stepValue = value
+            }
+
+            const sequenceValue = stepAnimation(stepValue, stepConfig, callback)
+            if (stepDelay != null) {
+              return withDelay(stepDelay, sequenceValue)
+            }
+            return sequenceValue
+          })
+          .filter(Boolean)
+
+        if (isTransform(key)) {
+          // we have a sequence of transforms
+          final['transform'] = final['transform'] || []
+
+          if (sequence.length) {
+            const transform = {}
+
+            transform[key] = withSequence(sequence[0], ...sequence.slice(1))
+
+            // @ts-ignore
+            final['transform'].push(transform)
+          }
+        } else {
+          // we have a normal sequence of items
+          // shadows not supported
+          if (sequence.length) {
+            final[key] = withSequence(sequence[0], ...sequence.slice(1))
+          }
+        }
+      } else if (isTransform(key)) {
+        final['transform'] = final['transform'] || []
+        // const transformKey = Object.keys(transformProp)[0]
+        // const transformValue = transformProp[transformKey]
+
+        if (transition?.[key]?.delay != null) {
+          delayMs = transition?.[key]?.delay
+        }
+
+        const transform = {}
+        let finalValue = animation(value, config, callback)
+        if (shouldRepeat) {
+          finalValue = withRepeat(finalValue, repeatCount, repeatReverse)
+        }
+        if (delayMs != null) {
+          transform[key] = withDelay(delayMs, finalValue)
+        } else {
+          transform[key] = finalValue
+        }
+
+        // @ts-ignore
+        final['transform'].push(transform)
+      } else if (typeof value === 'object') {
+        // shadows
+        final[key] = {}
+        Object.keys(value).forEach((innerStyleKey) => {
+          let finalValue = animation(value, config, callback)
+
+          if (shouldRepeat) {
+            finalValue = withRepeat(finalValue, repeatCount, repeatReverse)
+          }
+
+          if (delayMs != null) {
+            final[key][innerStyleKey] = withDelay(delayMs, finalValue)
+          } else {
+            final[key][innerStyleKey] = finalValue
+          }
+        })
+      } else {
+        let finalValue = animation(value, config, callback)
+        if (shouldRepeat) {
+          finalValue = withRepeat(finalValue, repeatCount, repeatReverse)
+        }
+
+        if (delayMs != null && typeof delayMs === 'number') {
+          final[key] = withDelay(delayMs, finalValue)
+        } else {
+          final[key] = finalValue
+        }
+      }
+    })
+
+    return final
+  })
+
+  useEffect(() => {
+    isMounted.value = true
+  }, [isMounted])
+
+  useEffect(
+    function allowUnMountIfMissingExit() {
+      if (!isPresent && !hasExitStyle) {
+        safeToUnmount?.()
+      }
+    },
+    [hasExitStyle, isPresent, safeToUnmount]
+  )
+
+  return {
+    style,
+  }
+}
